cd053c7e312e869d3319073044eb6c054a61bf35
Drzezo55 <158335353+Drzezo55@users.noreply.github.com>
2025-07-21 22:44:12 +0300

    Update tcga_download.R

--- tcga_download.R
+++ tcga_download.R
@@ -18,17 +18,15 @@ test_tab <- read.table(file = file_list[1], sep = '\t', header = TRUE)
 # Delete header lines that don't contain usefull information
 test_tab <- test_tab[-c(1:4),]
 # STAR counts and tpm datasets
-tpm_data_frame <- data.frame(test_tab[,1])
 count_data_frame <- data.frame(test_tab[,1])
 
 # Append cycle to get the complete matrix
-for (i in c(1:length(file_list))) {
+for (x in c(1:length(file_list))) {
   # Read table
-  test_tab <- read.table(file = file_list[i], sep = '\t', header = TRUE)
+  test_tab <- read.table(file = file_list[x], sep = '\t', header = TRUE)
   # Delete not useful lines
   test_tab <- test_tab[-c(1:4),]
   # Column bind of tpm and counts data
-  tpm_data_frame <- cbind(tpm_data_frame, test_tab[,7]) # 7 refers to the location where tpm present
   count_data_frame <- cbind(count_data_frame, test_tab[,4]) # 4 refers to the location where count present 
   # Print progres from 0 to 1
   print(i/length(file_list))
