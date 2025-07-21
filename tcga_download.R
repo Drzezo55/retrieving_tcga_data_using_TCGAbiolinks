e486e275f2df8f331b27fc63b6acf850a288e349
Abdelaziz Awad <abdelazizawad@Abdelazizs-MacBook-Pro.local>
2025-07-21 17:18:02 +0300

    r script

---
+++ tcga_download.R
@@ -0,0 +1,35 @@
+# Install TCGAbiolinks
+BiocManager::install("TCGAbiolinks")
+library(TCGAbiolinks)
+project_name <- "TCGA-LAML"
+query <- GDCquery(project = project_name,
+                  data.category = "Transcriptome Profiling",
+                  data.type = "Gene Expression Quantification",
+                  experimental.strategy = "RNA-Seq",
+                  workflow.type = "STAR - Counts")
+
+metadata <- query[[1]][[1]]
+GDCdownload(query, method = "api")
+main_dir <- file.path("GDCdata", project_name)
+file_list <- file.path("GDCdata", project_name,list.files(main_dir,recursive = TRUE)) 
+test_tab <- read.table(file = file_list[1], sep = '\t', header = TRUE)
+# Delete header lines that don't contain usefull information
+test_tab <- test_tab[-c(1:4),]
+# STAR counts and tpm datasets
+tpm_data_frame <- data.frame(test_tab[,1])
+count_data_frame <- data.frame(test_tab[,1])
+
+# Append cycle to get the complete matrix
+for (i in c(1:length(file_list))) {
+  # Read table
+  test_tab <- read.table(file = file_list[i], sep = '\t', header = TRUE)
+  # Delete not useful lines
+  test_tab <- test_tab[-c(1:4),]
+  # Column bind of tpm and counts data
+  tpm_data_frame <- cbind(tpm_data_frame, test_tab[,7]) # 7 refers to the location where tpm present
+  count_data_frame <- cbind(count_data_frame, test_tab[,4]) # 4 refers to the location where count present 
+  # Print progres from 0 to 1
+  print(i/length(file_list))
+}
+
+
